8cd56e6e62700eda8373553ad3bc2fa4c9d4c114
Vincent <vincentgordy@gmail.com>
2023-01-12 19:01:39 -0500

    Script for items in blocks
    Added logic for items to be picked up and add lives and coins. next will be to add sprite change logic to mario for magic mushroom

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+   //script will be used for the power up logic for interacting with mario
+   public enum Type
+    {
+        Coin,
+        ExtraLife,
+        MagicMuchroom,
+        StarPower,
+    }
+
+    //logic for giving the actual game object a type 
+    public Type type;
+
+    //we will use the box collider to know when we have entered the item
+    //we could use the circle collider but we want the physics to be handled seprately
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //lets check what object we are colliding with 
+        if (collision.CompareTag("Player"))
+        {
+            //when we collect we need to switch on what type it is
+            //we are calling collect on mario
+            Collect(collision.gameObject);
+
+        }
+
+
+
+    }
+
+
+    //we also want to pass a reference to the player so we can make the accoring changes to mario
+    //collect will look at mario and run the according Enum
+    private void Collect(GameObject player)
+    {
+        switch (type)
+        {
+            case Type.Coin:
+                //this is looking at current Instance of game manager and running AddCoin
+                GameManager.Instance.AddCoin();
+                
+                break;
+            case Type.ExtraLife:
+                GameManager.Instance.AddLife();
+                break;
+            case Type.MagicMuchroom:
+
+                //todo
+                break;
+            case Type.StarPower:
+
+                //todo
+                break;
+
+        }
+        //after its been collected destroy it
+        Destroy(gameObject);
+    }
+
+
+
+
+}
